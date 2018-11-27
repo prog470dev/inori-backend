6443df35fb74bd8c224464d6c4a681853d23a8da
prog470dev <tshimizudev4@gmail.com>
2018-11-28 02:20:47 +0900

    test

--- model/driver_test.go
+++ model/driver_test.go
@@ -22,10 +22,10 @@ func TestModelDriver(t *testing.T) {
 		t.Fatalf("failed %s", err)
 	}
 
-	_, err = dbx.Exec("create database ino;")
-	if err != nil {
-		t.Fatalf("failed %s", err)
-	}
+	//_, err = dbx.Exec("create database ino;")
+	//if err != nil {
+	//	t.Fatalf("failed %s", err)
+	//}
 
 	_, err = dbx.Exec("use ino;")
 	if err != nil {
