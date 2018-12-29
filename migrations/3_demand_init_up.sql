9caba0fd189a2dba4184c238d20918ff8539f5ac
prog470dev <tshimizudev4@gmail.com>
2018-12-30 01:03:49 +0900

    #28 modify table

--- migrations/3_demand_init_up.sql
+++ migrations/3_demand_init_up.sql
@@ -1,25 +1,13 @@
 CREATE TABLE ino.demand_school (
     rider_id    INT NOT NULL,
     day         INT NOT NULL,
+    dir         INT NOT NULL,
     start       INT NOT NULL,
     end         INT NOT NULL,
     FOREIGN KEY(rider_id) REFERENCES riders (id)
 );
 
-CREATE TABLE ino.demand_home (
-    rider_id    INT NOT NULL,
-    day         INT NOT NULL,
-    start       INT NOT NULL,
-    end         INT NOT NULL,
-    FOREIGN KEY(rider_id) REFERENCES riders (id)
-);
-
-CREATE TABLE ino.demand_aggregate_school (
-    time_zone   INT NOT NULL,
-    value       INT NOT NULL
-);
-
-CREATE TABLE ino.demand_aggregate_home (
+CREATE TABLE ino.demand_aggregate (
     time_zone   INT NOT NULL,
     value       INT NOT NULL
 );
